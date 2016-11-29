741f28b57fb826bb25e00811f58e143ad0e5b08f
Szymon Kuchnia <szymon.kuchnia@dpksystem.pl>
2016-11-29 06:06:59 +0100

    step_6

--- Activity2Example/app/src/main/java/com/example/agh/activity2example/MainActivity.java
+++ Activity2Example/app/src/main/java/com/example/agh/activity2example/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.agh.activity2example;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
 
         /* Uruchamiamy */
         startActivityForResult(intent, -1);
+    }
+
+    public void openBrowser(View view) {
+        /* ACTION_VIEW to stała, która mówi: chcę wywołać dowolną aktywność,
+         * która potrafi pokazać użytkownikowi dane, które zamieściłem w intencji */
+        Intent i = new Intent(Intent.ACTION_VIEW);
+
+        i.setData(Uri.parse("http://www.agh.edu.pl"));
+
+        startActivity(i);
     }
 }
